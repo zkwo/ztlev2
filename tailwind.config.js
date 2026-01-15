14aaf5dda2479f12384b39b71dd91c02149d772b
zkwo <ko40c980ew632@gmail.com>
2026-01-15 22:03:27 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      animation: {
-        'spin-slow': 'spin 8s linear infinite',
-      },
-      fontFamily: {
-        sans: ['Inter', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
-      }
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 }
